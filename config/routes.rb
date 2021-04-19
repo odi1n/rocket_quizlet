f7614ae4f5565da77c9dd357d22d2d6b23087299
odi1n <sashf1999@mail.ru>
2021-04-19 09:36:35 +0300

    - сделана страница категория - сделана страница категории - добавлены стили - добавлены route - контролер категорий

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'test/index'
-  devise_for :users
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  mount Ckeditor::Engine => '/ckeditor'
+    get 'test/index'
+    devise_for :users
+    mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+    mount Ckeditor::Engine => '/ckeditor'
 
-  #get 'search' => 'search#index', as: :search
+    #get 'search' => 'search#index', as: :search
 
-  root to: 'home#index'
+    root to: 'home#index'
+
+    resources :categories do
+
+    end
 end
