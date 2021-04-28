8125276e2c1462852dfc08f0f901d57e0b30e1e6
odi1n <sashf1999@mail.ru>
2021-04-28 14:35:14 +0300

    Составлено письмо

--- app/mailers/category_mailer.rb
+++ app/mailers/category_mailer.rb
@@ -2,7 +2,7 @@ class CategoryMailer < ApplicationMailer
 
     def open_email(user)
         @user = user
-        mail(to: "test@gmail.ru", subject: 'Welcome to My Awesome Site')
+        mail(to: "test@gmail.ru", subject: 'Проверка знаний для открытия категории')
     end
 
 end
