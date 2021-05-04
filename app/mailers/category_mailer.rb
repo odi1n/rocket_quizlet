6ff4fbcb2d898e964944c21d50e25005898f10d8
odi1n <sashf1999@mail.ru>
2021-05-04 14:31:50 +0300

    Уведомление админа о новой подаче заявки на тест

--- app/mailers/category_mailer.rb
+++ app/mailers/category_mailer.rb
@@ -1,8 +1,12 @@
 class CategoryMailer < ApplicationMailer
 
-    def open_email(user)
+    def open_email(user, test_report)
         @user = user
-        mail(to: "test@gmail.ru", subject: 'Проверка знаний для открытия категории')
+        @test_report = test_report
+        user = User.find_by(role: :admin)
+        if user.email
+            mail(to: user.email, subject: 'Проверка знаний для открытия категории')
+        end
     end
 
 end
